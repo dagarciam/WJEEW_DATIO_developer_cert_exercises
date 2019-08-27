95e24466089b79d943ef2104a76564c765e4c531
isaacvincent.rodriguez <isaacvincent.rodriguez@bbva.com>
2019-08-27 12:56:12 -0500

    [UPDATE]Updated branch with essential configuration

--- testAT/src/test/scala/com/bbva/datioamproduct/fdevdatio/RunCukesTest.scala
+++ testAT/src/test/scala/com/bbva/datioamproduct/fdevdatio/RunCukesTest.scala
@@ -3,38 +3,17 @@ package com.bbva.datioamproduct.fdevdatio
 import cucumber.api.CucumberOptions
 import cucumber.api.junit.Cucumber
 import org.junit.runner.RunWith
-import org.junit.{AfterClass, BeforeClass}
-
-import scala.sys.process._
-
-object RunCukesTest {
-
-  @BeforeClass
-  def startHDFS(): Unit = {
-
-    val scriptStartPath = ClassLoader.getSystemResource("docker/start.sh").getPath
-
-    "chmod +x " + scriptStartPath !
-
-    scriptStartPath !
-  }
-
-  @AfterClass
-  def stopHDFS(): Unit = {
-
-    val scriptStopPath = ClassLoader.getSystemResource("docker/stop.sh").getPath
-
-    "chmod +x " + scriptStopPath !
-
-    scriptStopPath !
-  }
-
-}
 
 @RunWith(classOf[Cucumber])
-@CucumberOptions(features = Array(
-  //"src/test/resources/acceptanceTests/DeveloperCertExercises.feature"
-),
+@CucumberOptions(
+  features = Array
+  (
+    "src/test/resources/features/DeveloperCertExercises.feature"
+  ),
+  glue = Array("com.datio.spark.bdt.steps", "com.bbva.datioamproduct.fdevdatio.developercertexercises"),
+  plugin = Array(
+    "pretty"
+  ),
   tags = Array("~@ignore")
 )
 class RunCukesTest
